8578473053d73dc5ef130d6a4a94ee8a94fda411
devtox <frank@fastmail.org>
2022-04-12 01:10:56 +0200

    update index

--- source/index.blade.php
+++ source/index.blade.php
@@ -1,21 +1,10 @@
 @extends('_layouts.master')
 
 @section('content')
-    <h1>Welcome to Artisan Static</h1>
+    <h1>Welcome to python.bond</h1>
 
-    <p>Artisan Static is a starter template for building a static Jigsaw blog hosted on Netlify.</p>
+    <p>Python is a programming language with many features, such as an intuitive syntax and powerful data structures. It's no wonder that this versatile language is one of the most popular coding languages in the world.</p>
 
-    <p>This comes with code highlighting, share buttons, comments, analytics, an RSS feed, a contact form, a CMS and more.</p>
+    <p>Fortunately, there are plenty of resources available to help you learn Python. In this blog I write Python tutorials. Enjoy!</p>
 
-    <p>The HTML, CSS and JavaScript in this template are extremely minimal, which makes the code easy to build on top of or replace completely.</p>
-
-    <a href="https://ko-fi.com/Y8Y07ACJ" target="_blank" rel="noopener">
-        <img src="{{ media('kofi-button.png') }}" alt="ko-fi" width="223" height="30">
-    </a>
-
-    <h2 id="test">Asset Compilation Test</h2>
-
-    <p>CSS test: <span class="test-css">text with blue border</span></p>
-
-    <p>JavaScript test: <a href="#test" class="test-js">click me</a></p>
 @endsection
